e4bb09dcf44d5b272f9543bca2f64c4052659401
Niraj <niraj.patel@flatironschool.com>
2023-01-30 09:24:51 -0500

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,70 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate',
+        23,
+        'M',
+        'Brown',
+        'Mean',
+        FALSE);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy',
+        23,
+        'M',
+        'Brown',
+        'Mean',
+        FALSE);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha',
+        23,
+        'F',
+        'Brown',
+        'Mean',
+        TRUE);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown',
+        45,
+        'M',
+        'Brown',
+        'Mean',
+        TRUE);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa',
+        23,
+        'F',
+        'Brown',
+        'Mean',
+        TRUE);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch',
+        23,
+        'M',
+        'Green',
+        'Mean',
+        FALSE);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy',
+        23,
+        'F',
+        'Red',
+        'Mean',
+        TRUE);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL,
+        23,
+        'M',
+        'Brown',
+        'Mean',
+        FALSE);
